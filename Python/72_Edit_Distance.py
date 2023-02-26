e981c6e910a1dc5aceda10cc773e81b3a9868c22
Harish <harishbharish6@gmail.com>
2023-02-26 17:14:05 +0530

    Added

---
+++ Python/72_Edit_Distance.py
@@ -0,0 +1,37 @@
+# Given two strings word1 and word2, return the minimum number of operations required to convert word1 to word2.
+
+# You have the following three operations permitted on a word:
+
+# Insert a character
+# Delete a character
+# Replace a character
+ 
+
+# Example 1:
+
+# Input: word1 = "horse", word2 = "ros"
+# Output: 3
+# Explanation: 
+# horse -> rorse (replace 'h' with 'r')
+# rorse -> rose (remove 'r')
+# rose -> ros (remove 'e')
+
+# https://www.youtube.com/@techwired8/?sub_confirmation=1
+
+class Solution:
+    def minDistance(self, word1: str, word2: str) -> int:
+        
+        m, n = len(word1), len(word2)
+
+        dp =[[0] * (n+1) for _ in range (m + 1)]
+
+        for i in range(m+1):
+            for j in range(n+1):
+
+                if i == 0: dp[i][j] = j
+                elif j == 0: dp[i][j] = i
+                elif word1[i-1] == word2[j-1]:
+                    dp[i][j] = dp[i-1][j-1]
+                else:
+                    dp[i][j] = min(dp[i-1][j-1], dp[i][j-1], dp[i-1][j]) + 1
+        return dp[m][n]
